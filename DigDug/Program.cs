26a5710ef2312a63a81409fa5e4a34e0cc6490eb
Dustin Schimel <dustin.schimel@gmail.com>
2018-05-02 13:23:45 -0600

    Added icons

--- DigDug/Program.cs
+++ DigDug/Program.cs
@@ -1,4 +1,4 @@
-﻿#region Using Statements
+#region Using Statements
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,11 +20,11 @@ namespace DigDug
 	static class Program
 #endif
 	{
-		private static Game1 game;
+		private static DigDug game;
 
 		internal static void RunGame()
 		{
-			game = new Game1();
+			game = new DigDug();
 			game.Run();
 #if !__IOS__ && !__TVOS__
 			game.Dispose();
